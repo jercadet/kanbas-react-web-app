253421a423706e5768b1c268053d7e0151e7a974
cadetjer <cadet.j@northeastern.edu>
2024-02-27 16:35:51 -0500

    on 2.5 todo list in lab section

--- src/Labs/a4/ReduxExamples/index.tsx
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import HelloReducer from "./HelloRedux/helloReducer";
 import HelloRedux from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux";
 
 const ReduxExamples = () => {
     return (
         <div>
             <h2>Redux Examples</h2>
             <HelloRedux />
+            <CounterRedux />
+            <AddRedux />
         </div>
     );
 };
