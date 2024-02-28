2ed92153b5e4503243aed6827f22b14a18e147f7
cadetjer <cadet.j@northeastern.edu>
2024-02-27 22:47:52 -0500

    Done with lab portion

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -5,9 +5,12 @@ import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
 import Highlight from "./Highlight";
 import Add from "./Add";
-import TodoList from "./todos/TodoList";
+// import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 
 function Assignment3() {
+    const { todos } = useSelector((state: LabState) => state.todosReducer);
     return (
         <div>
             <h1>Assignment 3</h1>
@@ -21,7 +24,14 @@ function Assignment3() {
                 vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
             </Highlight>
             <Add a={3} b={4} />
-            <TodoList />
+            {/* <TodoList /> */}
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
